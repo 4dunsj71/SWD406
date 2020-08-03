28e083201d48a71d3310866bd911003ac602f1df
4dunsj71 <4dunsj71@solent.ac.uk>
2020-08-03 18:02:23 +0100

    added resub to git

--- assignment work/practical work/python/menu.py
+++
@@ -1,7 +0,0 @@
-
-import sqlite3
-#declaring location of database
-db = sqlite3.connect('H:/documents/other applications/SQLiteStudio/orinoco.db')
-cursor = db.cursor()
-#prompting user for shopper id
-shopper_id = input("Enter Your Shopper ID: ")
