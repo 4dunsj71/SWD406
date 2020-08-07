70c983071f61d37e8daf3b5c73699beba0324386
4dunsj71 <4dunsj71@solent.ac.uk>
2020-08-07 15:58:21 +0100

    finishing up part one question 3

--- assignment work/practical work/RESUBMISSION/task3.py
+++ assignment work/practical work/RESUBMISSION/task3.py
@@ -1,40 +1,58 @@
 #!/usr/bin/python
 import sqlite3
-from sqlite3 import Error
 
-
-def create_connection(orinoco):
-    """ create a database connection """
-    conn = None
-    try:
-        conn = sqlite3.connect('orinoco.db')
-        print("connection succeeded")
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-if __name__=='__main__':
-    create_connection(r"orinoco.db")
-        
-
+db = sqlite3.connect('orinoco.db')
 
 shopperId = input("enter your shopper ID number: ")
 loopval = 0
+
 print ("\n")
 while loopval == 0:
-    if num.isdigit():
+    if shopperId.isdigit():
 
-        menuInput = input()
-
-
-
-        print('ORINOCO - SHOPPER MAIN MENU\n _______________________________________________ \n 1.Display your order history \n 2.add an item to your basket \n 3.view your basket \n 4.checkout \n 5.exit')
-
+        print('ORINOCO - SHOPPER MAIN MENU\n _______________________________________________ \n 1.Display your order history \
+             \n 2.add an item to your basket \
+              \n 3.view your basket \n 4.checkout \n 5.exit')
+        menuInput = input('select an option:')
         if menuInput == '1':
+            cursor = db.cursor()
             print('ORDER HISTORY \n _______________________________________________')
-            cursor.execute(sql [SELECT order_id ,delivery_address_id,payment_card_id, order_date,order_status FROM shopper_order WHERE shopper_id = (?)] shopperId )
-
+            sql_query ="SELECT op.order_id, \
+                STRFTIME('%d-%m-%Y',so.order_date),\
+                p.product_description,\
+                s.seller_name,\
+                op.price,\
+                op.quantity,\
+                so.order_status\
+                FROM ordered_products op \
+                INNER JOIN\
+                shopper_orders so ON op.order_id = so.order_id\
+                INNER JOIN\
+                products p ON op.product_id = p.product_id\
+                INNER JOIN\
+                sellers s ON s.seller_id = op.seller_id\
+                WHERE shopper_id = (?)\
+                ORDER BY so.order_date DESC"
+            cursor.execute(sql_query,(shopperId,))
+            all_rows = cursor.fetchall()
+            print("     order ID   order date        product                 seller         price       quantity  order status\n")
+            for row in all_rows:
+                order_id = row[0]
+                order_date = row[1]
+                product_description = row[2]
+                seller_name = row[3]
+                price = row [4]
+                quantity = row[5]
+                order_status = row[6]
+                print("{0:12}\t{1:}\t{2:18}\t{3:14}\t£{4:7.2f}\t{5:2}\t{6:12}".format(order_id, order_date[:10], product_description[:15], seller_name[:14], price, \
+                quantity, order_status[:8]))
+                cursor.close()
+            leavemenu = input("return to main menu? if you select no, the program will terminate(y/n)")
+            if leavemenu == 'y':
+                loopval = 0
+            elif leavemenu == 'n':
+                print('exiting program...')
+                loopval = 1
         elif menuInput == '2':
             print('')
 
@@ -45,7 +63,8 @@ while loopval == 0:
             print('')
 
         elif menuInput == '5':
-            print('')
+            loopval = 1
+            print('exiting program...')
 
     else:
         print("entered value is not numerical, please try again")
