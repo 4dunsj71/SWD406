a8985e378a2fe536c297f3204779fe981418bea6
4dunsj71 <4dunsj71@solent.ac.uk>
2020-08-07 18:11:21 +0100

    well this is fucked, how the hell do i check if a returned row is empty? SQLite3 in python sucks dick

--- assignment work/practical work/RESUBMISSION/task3.py
+++ assignment work/practical work/RESUBMISSION/task3.py
@@ -2,39 +2,39 @@
 import sqlite3
 
 db = sqlite3.connect('orinoco.db')
-
-shopperId = input("enter your shopper ID number: ")
+sql_query ="SELECT IFNULL(op.order_id,'empty'), \
+            STRFTIME('%d-%m-%Y',so.order_date),\
+            p.product_description,\
+            s.seller_name,\
+            op.price,\
+            op.quantity,\
+            so.order_status\
+            FROM ordered_products op \
+            INNER JOIN\
+            shopper_orders so ON op.order_id = so.order_id\
+            INNER JOIN\
+            products p ON op.product_id = p.product_id\
+            INNER JOIN\
+            sellers s ON s.seller_id = op.seller_id\
+            WHERE shopper_id = (?)\
+            ORDER BY so.order_date DESC"
 loopval = 0
 
 print ("\n")
 while loopval == 0:
+    cursor = db.cursor()
+    shopperId = input("enter your shopper ID number: ")
     if shopperId.isdigit():
-
         print('ORINOCO - SHOPPER MAIN MENU\n _______________________________________________ \n 1.Display your order history \
              \n 2.add an item to your basket \
               \n 3.view your basket \n 4.checkout \n 5.exit')
         menuInput = input('select an option:')
         if menuInput == '1':
             cursor = db.cursor()
-            print('ORDER HISTORY \n _______________________________________________')
-            sql_query ="SELECT op.order_id, \
-                STRFTIME('%d-%m-%Y',so.order_date),\
-                p.product_description,\
-                s.seller_name,\
-                op.price,\
-                op.quantity,\
-                so.order_status\
-                FROM ordered_products op \
-                INNER JOIN\
-                shopper_orders so ON op.order_id = so.order_id\
-                INNER JOIN\
-                products p ON op.product_id = p.product_id\
-                INNER JOIN\
-                sellers s ON s.seller_id = op.seller_id\
-                WHERE shopper_id = (?)\
-                ORDER BY so.order_date DESC"
             cursor.execute(sql_query,(shopperId,))
-            all_rows = cursor.fetchall()
+            result = all_rows = cursor.fetchall()
+            data = cursor.fetchone()
+            print('ORDER HISTORY \n _______________________________________________')
             print("     order ID   order date        product                 seller         price       quantity  order status\n")
             for row in all_rows:
                 order_id = row[0]
@@ -46,7 +46,9 @@ while loopval == 0:
                 order_status = row[6]
                 print("{0:12}\t{1:}\t{2:18}\t{3:14}\t£{4:7.2f}\t{5:2}\t{6:12}".format(order_id, order_date[:10], product_description[:15], seller_name[:14], price, \
                 quantity, order_status[:8]))
-                cursor.close()
+                if cursor.fetchone() == None:
+                    print('no orders found for the provided shopper ID')
+
             leavemenu = input("return to main menu? if you select no, the program will terminate(y/n)")
             if leavemenu == 'y':
                 loopval = 0
